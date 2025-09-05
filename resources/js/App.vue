51f088c8bf4f361d08ff38a58dd775609363195b
Oleh Grypas <o.grypas@student.tudelft.nl>
2025-09-05 17:32:47 +0200

    Header done

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,23 +1,73 @@
 <script setup>
-    import { ref, onMounted, computed } from 'vue';
+    import { ref, onMounted, onUnmounted, computed } from 'vue';
     import { useRoute } from 'vue-router'
     const route = useRoute();
     const isHome = computed(() => route.path === '/');
 
+    const screenWidth = ref(0)
+
+    function updateWidth() {
+    screenWidth.value = window.innerWidth
+    }
+
+    onMounted(() => {
+    updateWidth()
+    window.addEventListener('resize', updateWidth)
+    })
+
+    onUnmounted(() => {
+    window.removeEventListener('resize', updateWidth)
+    })
+
+    // Your breakpoint logic
+    const isLargeScreen = computed(() => screenWidth.value >= 1390)
+
 
 </script>
 
+<style>
+    .large {
+        display: flex;
+        justify-content: center;
+    }
+    .large > header {
+        align-self: center;
+        display: flex;
+        flex-grow: 1;
+        justify-content: space-between;
+        margin: 50px 50px 0px;
+        max-width: 1440px;
+    }
+    .logo {
+        width: 257px;
+    }
+    .large > header nav {
+        display: flex;
+        gap: 50px;
+        font-weight: 500;
+        font-size: 18px;
+    }
+
+    .video-background {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    }
+</style>
+
 <template>
-    <header class="flex flew-col">
-        <router-link to="/">
-            <img :src="isHome ? '/images/logow.png' : '/images/logob.png'" alt="Logo" />
-        </router-link>
-        <nav>
-            <router-link to="/work">Work</router-link>
-            <router-link to="/shorts">Shorts</router-link>
-            <router-link to="/about">About</router-link>
-            <router-link to="/contact">Contact</router-link>
-        </nav>
-    </header>
-    test
+    <div :class="isLargeScreen ? 'large' : 'small'">
+        <header>
+            <router-link class="logo" to="/">
+                <img class="logo" :src="isHome ? '/images/logow.png' : '/images/logob.png'" alt="Logo" />
+            </router-link>
+            <nav :class="[isHome ? 'text-white' : 'text-black']">
+                <router-link to="/work">Work</router-link>
+                <router-link to="/shorts">Shorts</router-link>
+                <router-link to="/about">About</router-link>
+                <router-link to="/contact">Contact</router-link>
+            </nav>
+        </header>
+    </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/work', function () {
+    return view('welcome');
+});
+
+Route::get('/shorts', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('welcome');
+});
