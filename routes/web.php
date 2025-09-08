7ecff2405b20d468b178d9f43bc92f8158c27d06
Oleh Grypas <o.grypas@student.tudelft.nl>
2025-09-08 09:05:13 +0200

    added auth, but it deleted all my FUCKING SHORTS WORK WHY???

--- routes/web.php
+++ routes/web.php
@@ -3,28 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Route::get('/work', function () {
-    return view('welcome');
-});
-
-Route::get('/shorts', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('welcome');
-});
-
-Route::get('/contact', function () {
-    return view('welcome');
-});
-
-Route::apiResource('videos', App\Http\Controllers\VideoController::class);
-Route::apiResource('shorts', App\Http\Controllers\ShortController::class);
-Route::apiResource('scrollImages', App\Http\Controllers\ImageController::class);
-Route::get('/test-images', function() {
-    return response()->json(['test' => 'working']);
-});
+require __DIR__.'/auth.php';
